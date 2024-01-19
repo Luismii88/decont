4b57b9a68be28230f945db6b65b76077d4419b08
Luis Miguel Viboras <lm.viboras@gmail.com>
2024-01-19 16:50:09 +0000

    Creamos script para descargar la secuencia (no definitivo)

--- scripts/download.sh
+++ scripts/download.sh
@@ -16,3 +16,45 @@
 #   CCAGGATTTACAGACTTTAAA
 #
 #   If $4 == "another" only the **first two sequence** should be output
+
+
+
+
+
+# Verifica si se proporciona el número correcto de argumentos
+if [ "$#" -lt 2 ]; then
+    echo "Uso: $0 <url_del_archivo> <directorio_salida> [descomprimir: si/no] [palabra_de_filtro]"
+    exit 1
+fi
+
+# Argumentos
+file_url=$1
+output_directory=$2
+uncompress=$3
+filter_word=$4
+
+# Crea el directorio de salida si no existe
+#con el -p hacemos que los directorios intermedios que no existan se creen
+#utilizo el $ para que el directorio se llame como el contenido de esa variable
+mkdir -p "$output_directory"
+
+# Extrae el nombre del archivo de la URL (sin el directorio)
+filename=$(basename "$file_url")
+
+# Descarga el archivo
+#el -O permite ponerle un nombre personalizado al archivo descargado
+wget -O "$output_directory/$filename" "$file_url"
+
+# Descomprime el archivo si se solicita
+if [ "$uncompress" == "yes" ]; then
+    gunzip -k "$output_directory/$filename"
+else
+    echo "No se ha descomprimido el archivo"
+fi
+
+# Filtra las secuencias basadas en la palabra especificada
+if [ -n "$filter_word" ]; then
+    grep -B 1 -v "$filter_word" "$output_directory/$filename" > "$output_directory/filtered_file"
+else
+    echo "No se han proporcionado condiciones para el filtrado"
+fi
