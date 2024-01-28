4df4fd656e41651d1b8a2638c87562a89580a344
Luis Miguel Viboras <luismii88bm@gmail.com>
2024-01-29 00:57:03 +0100

    index.sh mejorado

--- scripts/index.sh
+++ scripts/index.sh
@@ -7,11 +7,20 @@
 # STAR --runThreadN 4 --runMode genomeGenerate --genomeDir <outdir> \
 # --genomeFastaFiles <genomefile> --genomeSAindexNbases 9
 
+# Vamos a hacer una validación de argumentos como en todos los scripts
+if [ "$#" -ne 2 ]; then
+    echo "Uso: $0 <archivo_genoma> <directorio_salida_index>"
+    exit 1
+fi
 
 #Argumentos
 genome_file=$1
 output_directory=$2
 
+echo "Indexando el genoma en '$genome_file' en el directorio '$output_directory'..."
+
 #comando STAR para indexar
-STAR --runThreadN 4 --runMode genomeGenerate --genomeDir "$output_directory" \
+STAR --runThreadN 2 --runMode genomeGenerate --genomeDir "$output_directory" \
  --genomeFastaFiles "$genome_file" --genomeSAindexNbases 9
+
+echo "Indexación completada con éxito"
